dfab1d1ada87db3804b1cd31105ccf1d2e812176
= <=>
2024-03-21 09:21:10 -0300

    Ajustes envio de email UserServiceManager

--- Application/Services/UserServiceManager.cs
+++ Application/Services/UserServiceManager.cs
@@ -25,21 +25,21 @@ public class UserServiceManager : IUserService
     public async Task<User> AddNewUserAsync(User user)
     {
         await _userRepository.Insert(user);
-        _emailAdapter.SendEmail("abruno36@gmail.com", "teste@email.com", "User was included with sucess...", "Added user");
+        _emailAdapter.SendEmail("abruno36@gmail.com", user.Email, "Usuário incluído com sucesso!!", "Added user" );
         return user;
     }
 
     public async Task<User> UpdateUserAsync(User user)
     {
         var userUpdated = await _userRepository.Update(user);
-        _emailAdapter.SendEmail("abruno36@gmail.com", "teste@email.com", "User was updated with sucess...", "Updated user");
+        _emailAdapter.SendEmail("abruno36@gmail.com", user.Email, "Usuário alterado com sucesso!!", "Updated user");
         return userUpdated;
     }
 
     public async Task<User> DeleteUserAsync(Guid id)
     {
         var userDeleted = await _userRepository.Delete(id);
-        _emailAdapter.SendEmail("abruno36@gmail.com", "teste@email.com", "User was deleted with sucess...", "Deleted user");
+        _emailAdapter.SendEmail("abruno36@gmail.com", userDeleted.Email, "Usuário excluído com sucesso!!", "Deleted user");
         return userDeleted;
     }
 }
